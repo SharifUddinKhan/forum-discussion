06fbc7ff95388bf75deea6ba5ed04d33773d2870
Sharif Uddin Khan <38166381+SharifUddinKhan@users.noreply.github.com>
2018-04-19 02:28:00 -0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,6 +35,7 @@
                     <th>
                         DELETE
                     </th>
+                    <th></th>
                 </thead>
                 <tbody>
                     
